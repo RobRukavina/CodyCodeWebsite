d3472b79974a556878b29cf071080031eaf1cef2
RobRukavina <rehabrob440@gmail.com>
2020-11-03 15:50:20 -0700

    fixed link bieng defined but never used

--- Landing/src/components/Pricing/pricing-box.js
+++ Landing/src/components/Pricing/pricing-box.js
@@ -1,5 +1,5 @@
 import React, { Component } from "react";
-import { Link } from "react-router-dom";
+// import { Link } from "react-router-dom";
 import { Col, CardBody } from "reactstrap";
 
 class PricingBox extends Component {
